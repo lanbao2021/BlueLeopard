bc0b9edd486b7d5598417a58d83315f220df1360
“lantongxue” <442391656@qq.com>
2021-09-07 23:28:24 +0800

    d

--- 918/2019-4.cpp
+++ 918/2019-4.cpp
@@ -2,6 +2,8 @@
 // Created by 蓝同学 on 2021/9/7.
 //
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
 using namespace std;
 
 class A{
@@ -17,8 +19,12 @@ public:
 int A::n=1;
 
 int main(){
-    A *a = new A(); // 执行构造函数，n=3
+    A *a;
+    cout << a << endl;
+    a = new A(); // 执行构造函数，n=3
     delete a; // 执行析构函数，n=0
+    int n = 20000;
+    while(n){n--;}
     cout << a << endl;
     cout << "n=" << a->getN() << endl; // 为啥这个a还能访问？？？？？？？？？？
     cout << a;
