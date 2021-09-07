9abbf9814498652e572940cc664eb8fdb77a8b53
“lantongxue” <442391656@qq.com>
2021-09-07 22:29:37 +0800

    A *a = new A()这个看不太懂

--- 918/2019-4.cpp
+++ 918/2019-4.cpp
@@ -1,4 +1,26 @@
 // 2019-4.cpp
 // Created by 蓝同学 on 2021/9/7.
 //
+#include <iostream>
+using namespace std;
 
+class A{
+    static int n;
+public:
+    A(){ n+=2; }
+    ~A(){ n-=3; }
+    int getN(){
+        return n;
+    }
+};
+
+int A::n=1;
+
+int main(){
+    A *a = new A(); // 执行构造函数，n=3
+    delete a; // 执行析构函数，n=0
+    cout << a << endl;
+    cout << "n=" << a->getN() << endl; // 为啥这个a还能访问？？？？？？？？？？
+    cout << a;
+    return 0;
+}
